c9c0cd2215f953029e9e9daa0e0c1efbcd1460b2
unknown <lixy0311@163.com>
2022-07-28 11:03:17 +0800

    code change

--- code/bash.py
+++ code/bash.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, help="the path of train_data")
     command_args = parser.parse_args()
-    os.system("python proxy/train.py --data_path %s" %(command_args.data_path))
-    os.system("python main.py --data_path %s" %(command_args.data_path))
+    os.system("python3 proxy/train.py --data_path %s" %(command_args.data_path))
+    os.system("python3 main.py --data_path %s" %(command_args.data_path))
