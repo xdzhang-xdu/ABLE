3deb6ea8bbfbbd20e2e5ff33e361dced72ca9683
xdzhang <zhangxiaodong@xidian.edu.cn>
2022-09-08 16:10:49 +0800

    add the debugging tool, a new work.

--- testing_engines/gflownet/path_config.py
+++ testing_engines/gflownet/path_config.py
@@ -6,6 +6,7 @@ path_args = AttrDict(
     {
         # testing result data
         "test_result_direct": "/data/xdzhang/apollo7/active+max/{}",
+        "debug_result_direct": "/data/xdzhang/apollo7/debug/{}",
         "spec_path": "rawdata/specs/spec_data.json",
         # data for training
         "train_data_path": "generator/data/testset/a_testset_for_{}.json",
